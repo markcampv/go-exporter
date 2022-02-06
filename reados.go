c80dde42bb0298d6b3a8784b14b56587596b4917
markcampv <mnmvincent@gmail.com>
2022-02-06 14:45:43 -0500

    Setting up http server for exporting metrics

--- reados.go
+++ reados.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"flag"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/version"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -12,4 +18,25 @@ func main() {
 	var bind string
 	flag.StringVar(&bind, "bind", "0.0.0.0:9104", "bind")
 	flag.Parse()
+
+	// ========================
+	// Register handler
+	// ========================
+	prometheus.Register(version.NewCollector("query_exporter"))
+
+	//Register http handler
+
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		h := promhttp.HandleFor(prometheus.Gatherers{
+			prometheus.DefaultGatherer,
+		}, promhttp.HandlerOpts{})
+		h.ServerHTTP(w, r)
+	})
+
+	// start server
+
+	log.Infof("Starting http server - %s", bind)
+	if err := http.ListenAndServe(bind, nil); err != nil {
+		log.Errorf("Failed to start http server: %s", err)
+	}
 }
